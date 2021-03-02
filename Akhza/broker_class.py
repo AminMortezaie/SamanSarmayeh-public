ff5b61dbd0930fb7f0e87587750a9b67b32721ae
Amin Mortezaie <mohammadmortezaie@gmail.com>
2021-03-02 11:29:38 +0330

    Fix: Minor changes in broker class.
    adapt with broker controller class.

--- Akhza/broker_class.py
+++ Akhza/broker_class.py
@@ -33,13 +33,13 @@ class Broker:
                 EC.presence_of_element_located(
                     (By.XPATH, xpath)))
         if mode == "super_fast":
-            return WebDriverWait(self.driver, 0).until(
+            return WebDriverWait(self.driver, 5).until(
                 EC.presence_of_element_located(
                     (By.XPATH, xpath)))
 
     def create_basics(self):
         self.driver.get("https://mehr.exirbroker.com/login")
-        time.sleep(10)
+        time.sleep(5)
         self.loadElement("/html/body/div[2]/header/a").click()
         self.loadElement(
             "/html/body/div[1]/div[2]/div[1]/div/div[3]/div[1]/div[2]/div[1]/form/div[2]/input").send_keys("0311804421")
@@ -54,20 +54,44 @@ class Broker:
 
     def buy_stock(self, index, stock_id, price, volume):
         # choose stock
-        if([stock_id, price] not in self.bought_stocks):
-            self.driver.switch_to.window(
-                self.driver.window_handles[0])
+        self.driver.switch_to.window(
+            self.driver.window_handles[0])
+        flag = True
+
+        try:
             self.loadElement(
-                """//*[@id="marketViewTable"]/tbody/tr["""+str(stock_id)+"""]/td[1]/div[5]/div/div[2]/mat-icon[1]/i""").click()
-            self.loadElement("""//*[@id="orderPrice"]""").send_keys(price)
-            self.loadElement("""//*[@id="mat-input-18"]""").send_keys(volume)
-            self.loadElement("""//*[@id="buy_1"]/button[1]""").click()
+                """//*[@id="marketViewTable"]/tbody/tr["""+str(stock_id)+"""]/td[1]/div[5]/div/div[2]/mat-icon[1]/i""", mode='super_fast').click()
             self.loadElement(
-                """//*[@id="popupOrder1"]/div[1]/div[1]/button/span""").click()
-            self.bought_stocks.append([stock_id, price])
+                """//*[@id="orderPrice"]""", mode='super_fast').send_keys(price)
+            self.loadElement(
+                """//*[@id="mat-input-18"]""", mode='super_fast').send_keys(volume)
+            self.loadElement(
+                """//*[@id="buy_1"]/button[1]""", mode='super_fast').click()
+            self.loadElement(
+                """//*[@id="popupOrder1"]/div[1]/div[1]/button/span""", mode='super_fast').click()
+            flag = False
+        except:
             self.driver.refresh()
-            self.loadElement(
-                """//*[@id="mat-radio-7"]/label/div[1]/div[1]""").click()
+
+        self.loadElement(
+            """//*[@id="mat-radio-7"]/label/div[1]/div[1]""").click()
+        self.driver.switch_to.window(
+            self.driver.window_handles[index])
+
+    def sell_stock(self, index, stock_id, price, volume):
+        self.driver.switch_to.window(
+            self.driver.window_handles[0])
+        self.loadElement(
+            """//*[@id="marketViewTable"]/tbody/tr["""+str(stock_id)+"""]/td[1]/div[5]/div/div[2]/mat-icon[2]/i""").click()
+        self.loadElement("""//*[@id="orderPrice"]""").send_keys(price)
+        self.loadElement("""//*[@id="mat-input-18"]""").send_keys(volume)
+        self.loadElement("""//*[@id="buy_1"]/button[1]""").click()
+        self.loadElement(
+            """//*[@id="popupOrder1"]/div[1]/div[1]/button/span""").click()
+        self.bought_stocks.append([stock_id, price])
+        self.driver.refresh()
+        self.loadElement(
+            """//*[@id="mat-radio-7"]/label/div[1]/div[1]""").click()
 
         self.driver.switch_to.window(
             self.driver.window_handles[index])
